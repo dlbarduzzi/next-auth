5d2c6a5163091b6d9c1f897d96e26e7e1bb1d844
Dylan Barduzzi <dlbarduzzi@gmail.com>
2024-01-04 09:39:14 -0600

    Extend next-auth session and jwt callbacks

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,24 @@
+import type { DefaultSession } from "next-auth"
+
 import NextAuth from "next-auth"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 
-import { db } from "@/lib/db"
 import authConfig from "@/lib/auth.config"
+
+import { db } from "@/lib/db"
+import { getUserById } from "@/lib/data/user"
+
+type UserRole = "user" | "admin"
+
+type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole
+}
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser
+  }
+}
 
 export const {
   handlers: { GET, POST },
@@ -10,6 +26,34 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ token, session }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub
+      }
+
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole
+      }
+
+      return session
+    },
+    async jwt({ token }) {
+      if (!token.sub) {
+        return token
+      }
+
+      const user = await getUserById(token.sub)
+
+      if (!user) {
+        return token
+      }
+
+      token.role = user.role
+
+      return token
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
